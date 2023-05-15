999b86c9fb11c54c9f543e34549c4a8a363ae76c
AbacateBombado <ms.mazucato@gmail.com>
2023-05-15 15:49:33 -0300

    Finalizado

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Atividades-canvas
+Começamos pesquisando sobre canvas para entender melhor sobre o assunto, logo após fizemos um retangulo, circulo, linha, e linha curvada, e uma imagem, usando apenas 3 variavel e alguns comandos.
+
+Criamos um local especifico para guardar nossos objetos dentro de um retangulo onde nada afeta o local dos objetos.
+
+ 
--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,36 +1,30 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const img = document.getElementById("GERALDO");
-
-//quadrado
-ctx.fillStyle = "pink";
-ctx.fillRect(0, 180, 150, 150);
 //retangulo
 ctx.fillStyle = "#FF0000"
-ctx.fillRect (0, 100, 150, 75);
+ctx.fillRect (225, 0, 150, 75);
 //Circulo
 ctx.beginPath();
-ctx.arc(45, 50, 40, 0, 2 * Math.PI);
+ctx.arc(300, 135, 50, 0, 2 * Math.PI);
 ctx.stroke();
+
 //Linha
-ctx.moveTo(120, 75);
-ctx.lineTo(150, 70);
+ctx.moveTo(370, 220);
+ctx.lineTo(225, 220);
 ctx.stroke();
+
+
+
 //linha curva
 ctx.beginPath();
-ctx.moveTo(100,100);
-ctx.quadraticCurveTo(90,125,25,70);
-ctx.quadraticCurveTo(65,100,50,100);
-ctx.quadraticCurveTo(90,120,30,125);
-ctx.quadraticCurveTo(100,120,65,100);
-ctx.quadraticCurveTo(105,100,125,62.5);
-ctx.quadraticCurveTo(125,25,75,25);
+ctx.moveTo(370,280);
+ctx.quadraticCurveTo(390,340,270,280);
 ctx.stroke();
-//Triangulo
-ctx.beginPath();
-ctx.moveTo(150 ,75);
-ctx.lineTo(100,75);
-ctx.lineTo(100,25); 
-ctx.fill();
+
+
+
+
 //Imagem
-ctx.drawImage(img, 10, 10);
+ctx.moveTo(10, 10);
+ctx.drawImage(img, 5, 5);
